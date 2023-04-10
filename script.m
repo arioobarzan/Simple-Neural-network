87ab54f8b10d27b07ed107bbc1252929b4c56819
Arioo Barzan <arioobarzan@hotmail.com>
2023-04-10 16:06:19 +0330

    Update script.m

--- script.m
+++ script.m
@@ -2,27 +2,26 @@ clc
 clear all
 close all
 
-dataset  = csvread('data.csv');
-label    = csvread('label.csv');
+dataset = csvread('data.csv');
+label = csvread('label.csv');
 
-train_data  = dataset(:,1:500);
+train_data = dataset(:,1:500);
 train_label = label(1:500);
-test_data   = dataset(:,501:557);
-test_label  = label(501:557);
+test_data = dataset(:,501:557);
+test_label = label(501:557);
 
 
-[x,t,net]=Made_nf(dataset,label);
+[x,t,net] = Made_nf(dataset,label);
 
 iteration = 100;
 
 for i=0:iteration
-    [y,e,net]=nf_train(x,t,net);
+    [y,e,net] = nf_train(x,t,net);
     predict = net(test_data);
     real = test_label;
 
     error = abs(predict-real)./real;
     
-    mean(error);
     disp(['iteration: ',num2str(i),' - ','mean(error):',num2str(mean(error))])
 end
 h = plot(1:57,real(1,:),'blue',1:57,predict(1,:),'red');
